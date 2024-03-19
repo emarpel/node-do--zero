7f027f13b72ef08ca486940e252c4c01a70fda43
Buda <emarpel@gmail.com>
2024-03-19 13:25:47 -0300

    Ajustes de conexão

--- create-table.js
+++ create-table.js
@@ -1,13 +1,18 @@
-
 import { sql } from './db.js'
 
+/* sql`
+    DROP TABLE tabela;
+`.then(() => {
+    console.log('Drop Table OK')
+}) */
+
 sql`
-    CREATE TABLE Tabela (
-        id INT PRIMARY KEY AUTO_INCREMENT,
+    CREATE TABLE videos (
+        id TEXT PRIMARY KEY,
         title TEXT,
         description TEXT,
         duration INTEGER
     );
 `.then(() => {
-    console.log('Create Table OK');
+    console.log('Create Table OK')
 })
--- db.js
+++ db.js
@@ -4,6 +4,4 @@ import postgres from 'postgres'
 const { PGHOST, PGDATABASE, PGUSER, PGPASSWORD, ENDPOINT_ID } = process.env;
 const URL = `postgres://${PGUSER}:${PGPASSWORD}@${PGHOST}/${PGDATABASE}?options=project%3D${ENDPOINT_ID}`;
 
-const sql = postgres(URL, { ssl: 'require' });
-
-export default sql;
+export const sql = postgres(URL, { ssl: 'require' });
